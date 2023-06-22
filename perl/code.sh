50f6cc516f19ebc55c45d7917304fabd19f29e5e
Aravind <aravindhannew@gmail.com>
2023-06-22 08:39:06 +0530

    code added in code.sh perl code

---
+++ perl/code.sh
@@ -0,0 +1,15 @@
+#perl -e 'print "aravind" x 20'
+
+#echo "HI"
+
+
+#perl -e 'print "\x41" x 20'
+
+#perl -e 'print "A"x20 . "BCD" . "\x61\x66\x67\x69"x2 . "Z";'
+
+#echo "\nhi"
+
+
+$(perl -e 'print "uname";')
+
+una$(perl -e 'print "m";')e
