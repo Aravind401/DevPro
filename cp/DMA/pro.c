9faf40e337ccdfd6eb0ba466621c758f3a05b022
Aravind <aravindhannew@gmail.com>
2023-06-24 14:13:38 +0530

    direct memory access

---
+++ cp/DMA/pro.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+
+//printf("7th: %7$d, 4th: %4$05d\n", 10, 20, 30, 40, 50, 60, 70, 80);
+
+printf("%5$d, and next is %3$04d",10,20,30,40,50,60);
+
+exit(0);
+}
